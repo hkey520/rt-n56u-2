448c386f78e288bd74dbc21b41a561dbc8349558
HuangYingNing <HuangYingNing at google mail system>
2018-12-08 13:38:52 +0800

    fix Newifi3 D2 reset key; disable USB for WR1200JS.

--- trunk/configs/boards/NEWIFI3/board.h
+++ trunk/configs/boards/NEWIFI3/board.h
@@ -8,7 +8,7 @@
 #define BOARD_MODEL_URL		"http://www.newifi.com/"
 #define BOARD_BOOT_TIME		30
 #define BOARD_FLASH_TIME	120
-#undef BOARD_GPIO_BTN_FN1
+// #undef BOARD_GPIO_BTN_FN1
 #define BOARD_GPIO_BTN_RESET	3
 #define BOARD_GPIO_BTN_WPS	7
 #undef  BOARD_GPIO_LED_ALL
@@ -21,6 +21,8 @@
 #define BOARD_GPIO_LED_USB	10
 #undef BOARD_GPIO_LED_ROUTER
 #undef  BOARD_GPIO_PWR_USB
+#define BOARD_GPIO_PWR_USB	11
+#define BOARD_GPIO_PWR_USB_ON	1 /* 1: 5V Power ON, 0: 5V Power OFF */
 #define BOARD_HAS_5G_11AC	1
 #define BOARD_NUM_ANT_5G_TX	2
 #define BOARD_NUM_ANT_5G_RX	2
